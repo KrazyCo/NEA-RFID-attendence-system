96a4baae4e7ec1fa27014b4400ca3913f7776bed
KrazyCo <conorstrickland@hotmail.com>
2023-03-02 21:16:24 +0000

    bro

--- database/pullAllStudentActions.py
+++ database/pullAllStudentActions.py
@@ -5,11 +5,11 @@ def pullAllStudentActions():
     try:
         db = loadDatabase()
         c = db.cursor()
-        c.execute('''SELECT StudentIn.CardID, StudentIn.Time, "In" FROM StudentIn''')
+        c.execute('''SELECT StudentIn.CardID, StudentIn.Time, "In" FROM StudentIn''') # no linking records used
         studentIn = c.fetchall()
-        c.execute('''SELECT StudentOut.CardID, StudentOut.Time, "Out" FROM StudentOut''')
+        c.execute('''SELECT StudentOut.CardID, StudentOut.Time, "Out" FROM StudentOut''') # no linking records used
         studentOut = c.fetchall()
-        studentActions = studentIn + studentOut
+        studentActions = studentIn + studentOut # only need 2 dimensional list
         db.close()
     except Error as e:
         print(e)
