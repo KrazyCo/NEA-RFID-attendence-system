23dd94af98c4bd8e95a981687a1a20af94783d3e
KrazyCo <conorstrickland@hotmail.com>
2023-03-05 13:56:32 +0000

    jahbsdjhsbefr

--- database/pullAllStudentCards.py
+++ database/pullAllStudentCards.py
@@ -6,11 +6,12 @@ def pullAllStudentCards():
         db = loadDatabase()
         c = db.cursor()
         c.execute('''SELECT StudentCards.CardID, StudentCards.StudentID, Students.StudentName FROM StudentCards JOIN Students ON StudentCards.StudentID = Students.StudentID''')
+        # select all records from StudentCards table and join with Students table to get StudentName
         studentCards = c.fetchall()
         db.close()
     except Error as e:
         print(e)
-    return studentCards
+    return studentCards # returns 2 dimensional list
 
 if __name__ == '__main__':
     print(pullAllStudentCards())
